efd107533ba926c7fd53b00fa097469f7e44f095
Amartinezz23 <antoniomartinezcolmenero33@gmail.com>
2026-02-02 15:14:24 +0100

    Annado el componente de listarVideoJuegos con su css

--- src/ListarVideojuegos.jsx
+++ src/ListarVideojuegos.jsx
@@ -1,26 +1,36 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import './ListarVideojuegos.css';
 
-const ListarVideojuegos = ()=>{
-    
-    const [videojuego, setVideojuego] = useState([]);
-    
-    useEffect(()=>{
-        fetch(`http://localhost:3000/videojuegos`).then(response=> response.json())
-        .then(data=>setVideojuego(data))
-    },[])
-    
-    
-    return(
-        <div id="container"> 
-            {videojuego.map((juego, index)=>{
-                return(
-                    <div >
-                        <img id="imagen" src={juego.urlImagen} width={"100rem"} ></img>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+const ListarVideojuegos = () => {
+
+  const [videojuego, setVideojuego] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/videojuegos')
+      .then(response => response.json())
+      .then(data => setVideojuego(data));
+  }, []);
+
+  return (
+    <div className="container">
+      {videojuego.map((juego) => {
+        return (
+          <div className="juego-card" key={juego.id}>
+            <img
+              className="imagen"
+              src={juego.urlImagen}
+              alt={juego.nombre}
+            />
+            <h3>{juego.nombre}</h3>
+            <p>{juego.descripcion}</p>
+            <p>{juego.compañia}</p>
+            <p>Precio de salida <strong>{juego.precio}</strong></p>
+
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default ListarVideojuegos;
